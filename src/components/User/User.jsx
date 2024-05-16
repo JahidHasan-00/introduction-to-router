fac96fbc9738f159c9d086e5fc0a4ff46bc84587
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-05-16 10:21:05 +0600

    Show Details component is added using dynamic routing

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,20 +1,28 @@
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 const User = ({user}) => {
 
-    const {id, name, email, phone, website} = user;
-    console.log(user);
+    const {id, name, email, phone} = user;
+    // console.log(user);
 
     const userStyle = {
-        border: '5px solid orange',
+        border: '3px solid orange',
         padding: '10px',
-        borderRadius: '10px'
+        borderRadius: '20px',
+        background: 'lightgray'
     }
-
+    const hoverStyle = {
+        background: 'red'
+    }
     return (
-        <div style={userStyle}>
+        <div style={userStyle} className={{hover:`${hoverStyle}`}}>
             <h2>{name}</h2>
             <p>Email: {email}</p>
             <p>Phone: {phone}</p>
+            <Link to={`/user/${id}`}>Show Details</Link>
+            <Link to={`/user/${id}`}>
+                <button>Click Me</button>
+            </Link>
         </div>
     );
 };
